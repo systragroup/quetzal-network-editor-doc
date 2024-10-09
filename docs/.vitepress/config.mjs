8a1c8e8e8b86334b7d6690582d0bc8f8c58fa5e4
CHASSERIEAU Quentin <qchasserieau@systra.info>
2024-10-09 14:55:25 -0400

    enfrts

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig} from 'vitepress'
 import {shared} from './shared'
-
+import {en} from './en'
+import {fr} from './fr'
 
 export default defineConfig({
   ...shared,
@@ -8,12 +9,13 @@ export default defineConfig({
     root: {
       label: 'English',
       lang: 'en',
+      ...en
     },
     fr: {
       label: 'French',
       lang: 'fr', // optional, will be added  as `lang` attribute on `html` tag
-      link: '/fr' // default /fr/ -- shows on navbar translations menu, can be external
-      // other locale specific properties...
+      link: '/fr', // default /fr/ -- shows on navbar translations menu, can be external
+      ...fr // other locale specific properties...
     }
   }
 })
