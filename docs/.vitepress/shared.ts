2aaf56f042c294a9b306026e4918cae48653de61
CHASSERIEAU Quentin <qchasserieau@systra.info>
2024-10-09 14:41:56 -0400

    defineconfing

--- docs/.vitepress/shared.ts
+++ docs/.vitepress/shared.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
-export const shared({
+export const shared = defineConfig({
     title: "Quetzal Interface",
     description: "doc",
     appearance:false, //True to have dark mode choice
