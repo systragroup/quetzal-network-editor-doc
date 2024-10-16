91980c6c7d0b40bfd81e6d4b84d011e191c31c5a
CHASSERIEAU Quentin <qchasserieau@systra.info>
2024-10-16 16:33:06 -0400

    favicon stie :q

--- docs/.vitepress/en.ts
+++ docs/.vitepress/en.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export const en = defineConfig({
-    title: "Quetzal Interface",
+    title: "Quetzal Cloud",
     description: "doc",
     appearance:false, //True to have dark mode choice
     base:"/quetzal-network-editor-doc/",
--- docs/.vitepress/fr.ts
+++ docs/.vitepress/fr.ts
@@ -5,6 +5,17 @@ export const fr = defineConfig({
     description: "doc",
     appearance:false, //True to have dark mode choice
     base:"/quetzal-network-editor-doc/",
+    head: [
+      ['link', { rel: 'icon', type: 'image/svg+xml', href: '/vitepress-logo-mini.svg' }],
+      ['link', { rel: 'icon', type: 'image/png', href: '/vitepress-logo-mini.png' }],
+      ['meta', { name: 'theme-color', content: '#5f67ee' }],
+      ['meta', { property: 'og:type', content: 'website' }],
+      ['meta', { property: 'og:locale', content: 'en' }],
+      ['meta', { property: 'og:title', content: 'VitePress | Vite & Vue Powered Static Site Generator' }],
+      ['meta', { property: 'og:site_name', content: 'VitePress' }],
+
+    ],
+    
     themeConfig: {
       logo: 'favicon.png',
   
