1f3366295a52d4d026ada6b3f6056c2c28e30104
CHASSERIEAU Quentin <qchasserieau@systra.info>
2024-10-16 16:30:49 -0400

    test

--- docs/.vitepress/en.ts
+++ docs/.vitepress/en.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export const en = defineConfig({
+    title: "Quetzal Interface",
     description: "doc",
     appearance:false, //True to have dark mode choice
     base:"/quetzal-network-editor-doc/",
