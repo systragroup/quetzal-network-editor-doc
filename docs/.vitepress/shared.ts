f14cf97fce27ce22794c30ad1ba49fc27712b495
CHASSERIEAU Quentin <qchasserieau@systra.info>
2024-10-16 15:56:32 -0400

    favicon

--- docs/.vitepress/shared.ts
+++ docs/.vitepress/shared.ts
@@ -6,7 +6,10 @@ export const shared = defineConfig({
     description: "doc",
     appearance:false, //True to have dark mode choice
     base:"/quetzal-network-editor-doc/",
-    head: [['link', { rel: 'icon', type: 'image/png', href: '/favicon.png' }]],
+    head: [
+      ['link', { rel: 'icon', type: 'image/png', href: '/favicon.png' }],
+      ['link', { rel: 'icon', href: '/favicon.ico' }]
+    ],
     themeConfig: {
       logo: 'favicon.png',
       search: {
